ee2bf10995f074a892024e381ab5efa198edd17f
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-19 15:39:39 +0700

    Upgrade Module

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	say_hello "github.com/arifrachman98/say-hello"
+	say_hello "github.com/arifrachman98/say-hello/v2"
 )
 
 func main() {
-	fmt.Println(say_hello.SayHello())
+	fmt.Println(say_hello.SayHello("Hoke"))
 }
